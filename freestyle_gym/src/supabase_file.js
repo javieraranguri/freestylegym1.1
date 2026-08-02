9e7cb8e17e2693ab1ef8c03f36d1f6989e9e0c18
javieraranguri <javier.aranguri@gmail.com>
2026-08-02 14:13:12 -0300

    Delete freestyle_gym/src/supabase_file.js

--- freestyle_gym/src/supabase_file.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://veqfizjhfqfwridjycug.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlcWZpempoZnFmd3JpZGp5Y3VnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4NDgxNjMsImV4cCI6MjA5NjQyNDE2M30.yvQdFDxMpJuQ0aaLY2LHTUZ1z1WAPYhse6FCxFj7OCQ'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
